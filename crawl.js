c6095905115e64cf8f228bb47cc7d1cb07efa2a2
Ronald George <ronaldsamgeorge@gmail.com>
2022-09-19 19:41:34 +0530

    Normalize URL

--- crawl.js
+++ crawl.js
@@ -1,5 +1,16 @@
+
+
 function normalizeURL(url){
 
+    let lowerCasedURL = url.toLowerCase();
+    const lenOfURL = url.length;
+    
+    if (lowerCasedURL[lenOfURL -1] === '/'){
+        lowerCasedURL = lowerCasedURL.substr(0,lenOfURL-1);
+    }
+
+    const link = new URL(lowerCasedURL);
+    return (`${link.hostname}${link.pathname}`);
 }
 
 module.exports = {normalizeURL};
--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,31 @@
 const {normalizeURL} = require('./crawl.js');
 const {test, expect} = require('@jest/globals');
 
-test('normalize https://wagsLane.Dev/path', () => {
-    expect(normalizeURL('https://wagsLane.Dev/path')).toBe('wagsLane.Dev/path');
+test('normalize protocol', () => {
+    const input = 'https://wagsLane.dev/path';
+    const received = normalizeURL(input);
+    const expected = 'wagslane.dev/path'
+    expect(received).toBe(expected);
+});
+
+
+test('normalize slash', () => {
+    const input = 'https://wagslane.dev/path/';
+    const received = normalizeURL(input);
+    const expected = 'wagslane.dev/path';
+    expect(received).toBe(expected);
+});
+
+test('normalize capital Letters', () => {
+    const input = "https://wagsLane.Dev/path";
+    const received = normalizeURL(input);
+    const expected = "wagslane.dev/path";
+    expect(received).toBe(expected);
+});
+
+test('normalize http', () =>{
+    const input  = 'http://wagslane.dev/path';
+    const received = normalizeURL(input);
+    const expected = 'wagslane.dev/path';
+    expect(received).toBe(expected);
 });
